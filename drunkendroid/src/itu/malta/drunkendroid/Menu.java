81e4546286a372b846ef4c4d81ffb0d7b925d60b
snielsen <snielsen@3995579f-fdfd-45ad-a057-9d0aee8b8447>
2009-10-21 12:45:19 +0000

    Changed Main to Menu and added Google Maps to Android Manifest.
    git-svn-id: http://subversion.assembla.com/svn/drunkendroid/src@32 3995579f-fdfd-45ad-a057-9d0aee8b8447

--- drunkendroid/src/itu/malta/drunkendroid/Menu.java
+++ drunkendroid/src/itu/malta/drunkendroid/Menu.java
@@ -3,7 +3,7 @@ package itu.malta.drunkendroid;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Main extends Activity {
+public class Menu extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
