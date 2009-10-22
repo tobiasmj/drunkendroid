e0e0240873fdd9046519342febaddb93a6e05f31
Jeppebm <Jeppebm@3995579f-fdfd-45ad-a057-9d0aee8b8447>
2009-10-22 13:13:25 +0000

    Added UI layer and Settings activity
    git-svn-id: http://subversion.assembla.com/svn/drunkendroid/src@34 3995579f-fdfd-45ad-a057-9d0aee8b8447

---
+++ drunkendroid/src/itu/malta/drunkendroid/ui/AppSettings.java
@@ -0,0 +1,16 @@
+package itu.malta.drunkendroid.ui;
+
+import itu.malta.drunkendroid.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppSettings extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.appsettings);
+		
+	}
+
+}
