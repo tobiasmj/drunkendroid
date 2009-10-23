db2fd3e5540169d311fdb279f78aa9ed87981982
Jeppebm <Jeppebm@3995579f-fdfd-45ad-a057-9d0aee8b8447>
2009-10-23 09:10:01 +0000

    Name changes, Menu added to main window.
    git-svn-id: http://subversion.assembla.com/svn/drunkendroid/src@38 3995579f-fdfd-45ad-a057-9d0aee8b8447

--- drunkendroid/src/itu/malta/drunkendroid/ui/Main.java
+++ drunkendroid/src/itu/malta/drunkendroid/ui/Main.java
@@ -6,23 +6,27 @@ import itu.malta.drunkendroid.R.layout;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-public class Menu extends Activity {
+public class Main extends Activity {
+	
+	private static final int MENU_SETTINGS = Menu.FIRST;
+	private static final int MENU_PREVIOUS_TRIPS = Menu.FIRST + 1;
 	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu);
+        setContentView(R.layout.main);
         
         final Button mvBtn = (Button)findViewById(R.id.mapViewButton);
+        
         
     	mvBtn.setOnClickListener(new Button.OnClickListener()
     	{
     		public void onClick(View v) {
-    			// TODO Auto-generated method stub
     			try {
     				System.out.println("Start view map");
     				Intent i = new Intent(".VIEW_MAP");
@@ -34,4 +38,13 @@ public class Menu extends Activity {
     		}
     	});
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, Main.MENU_SETTINGS, 0, R.string.menu_settings).setIcon(android.R.drawable.ic_menu_preferences);
+		menu.add(0, Main.MENU_PREVIOUS_TRIPS, 0, R.string.menu_previous_trips);
+		openOptionsMenu();
+		return true;
+	}
 }
