d4781167420d9b3bb57f57afd0488fd84a170e71
slumhest <slumhest@3995579f-fdfd-45ad-a057-9d0aee8b8447>
2009-10-26 13:11:01 +0000

    Fixed: #2 allowing multiple connections to the database, each request gets it's own connection
    git-svn-id: http://subversion.assembla.com/svn/drunkendroid/src@52 3995579f-fdfd-45ad-a057-9d0aee8b8447

--- drunkendroid-server/src/drunkendroidserver/Util/DatabaseConnection.java
+++ drunkendroid-server/src/drunkendroidserver/Util/DatabaseConnection.java
@@ -4,14 +4,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-	private Connection conn = null;
+	private String connURL = null;
 	private DatabaseConnection(){
-		try { 
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/drunkendroid-server?user=drunkendroid&password=81sliema");
-			} catch (SQLException se) {
-				//TODO : handle exception
-			}
-		
+			connURL = "jdbc:mysql://localhost/drunkendroid-server?user=drunkendroid&password=81sliema";
+			//conn = DriverManager.getConnection("jdbc:mysql://localhost/drunkendroid-server?user=drunkendroid&password=81sliema");	
 	}
 	
 	private static class DatabaseConnectionSingleton {
@@ -22,7 +18,13 @@ public class DatabaseConnection {
 		return DatabaseConnectionSingleton.INSTANCE;
 	}
 	public Connection getConn(){
+		Connection conn = null;
+		try{
+			conn  = DriverManager.getConnection(connURL);
+			
+		} catch (SQLException se){
+			// ignore for new since we can't do much about it?
+		}
 		return conn;
-		
 	}
 }
