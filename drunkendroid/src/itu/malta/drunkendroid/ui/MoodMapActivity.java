6a310460fee1705c1a865a0f7e5f6d9ea064cfe8
snielsen <snielsen@3995579f-fdfd-45ad-a057-9d0aee8b8447>
2009-10-28 13:38:27 +0000

    Trying to get location from device.
    git-svn-id: http://subversion.assembla.com/svn/drunkendroid/src@55 3995579f-fdfd-45ad-a057-9d0aee8b8447

--- drunkendroid/src/itu/malta/drunkendroid/ui/MoodMapActivity.java
+++ drunkendroid/src/itu/malta/drunkendroid/ui/MoodMapActivity.java
@@ -7,10 +7,15 @@ import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView; 
  
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MoodMapActivity extends MapActivity {
-	
+	Location location;
+	LocationManager locationManager;
 	MapView mapView;
 	MapController mapController;
 	GeoPoint gp;
@@ -19,6 +24,24 @@ public class MoodMapActivity extends MapActivity {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+    	locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+    	
+    	Criteria criteria = new Criteria();
+    	criteria.setAccuracy(Criteria.ACCURACY_FINE);
+    	criteria.setAltitudeRequired(false);
+    	criteria.setBearingRequired(false);
+    	criteria.setCostAllowed(true);
+    	criteria.setPowerRequirement(Criteria.POWER_LOW);
+
+    	String provider = locationManager.getBestProvider(criteria, true);
+    	
+    	if (provider != null) {
+    		location = locationManager.getLastKnownLocation(provider);
+    		/* TODO - adjust update times to minimize battery use. Can be changed as needed.
+    		*
+    		*/
+    	} 
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
         
@@ -29,10 +52,11 @@ public class MoodMapActivity extends MapActivity {
         String coordinates[] = {"35.908138", "14.500975"};
         double lat = Double.parseDouble(coordinates[0]);
         double lng = Double.parseDouble(coordinates[1]);
- 
-        gp = new GeoPoint(
-            (int) (lat * 1E6), 
-            (int) (lng * 1E6));
+        System.out.println("GPS: " + location.getLatitude() + "," + location.getLongitude());
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        
+        gp = new GeoPoint((int)(lat * 1E6),(int)(lng * 1E6));
  
         mapController.animateTo(gp);
         mapController.setZoom(17); 
