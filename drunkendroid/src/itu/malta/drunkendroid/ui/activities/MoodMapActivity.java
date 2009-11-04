136411e0613042c74b09257182cc7db0e327a0de
snielsen <snielsen@3995579f-fdfd-45ad-a057-9d0aee8b8447>
2009-11-04 13:49:44 +0000

    MoodMap now showing current location - just for test purposes.
    git-svn-id: http://subversion.assembla.com/svn/drunkendroid/src@67 3995579f-fdfd-45ad-a057-9d0aee8b8447

--- drunkendroid/src/itu/malta/drunkendroid/ui/activities/MoodMapActivity.java
+++ drunkendroid/src/itu/malta/drunkendroid/ui/activities/MoodMapActivity.java
@@ -1,15 +1,20 @@
 package itu.malta.drunkendroid.ui.activities;
 
+import java.util.List;
+
 import itu.malta.drunkendroid.R;
+import itu.malta.drunkendroid.ui.map.MoodOverlay;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
-import com.google.android.maps.MapView; 
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
  
 import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
@@ -18,49 +23,103 @@ public class MoodMapActivity extends MapActivity {
 	LocationManager locationManager;
 	MapView mapView;
 	MapController mapController;
+	MoodOverlay moodOverlay;
 	GeoPoint gp;
+	int t = 5000;
+	int distance = 10;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+        
     	locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
     	
-    	Criteria criteria = new Criteria();
-    	criteria.setAccuracy(Criteria.ACCURACY_FINE);
-    	criteria.setAltitudeRequired(false);
-    	criteria.setBearingRequired(false);
-    	criteria.setCostAllowed(true);
-    	criteria.setPowerRequirement(Criteria.POWER_LOW);
-
-    	String provider = locationManager.getBestProvider(criteria, true);
-    	
+    	String provider = locationManager.getBestProvider(getCriteria(), true);
+    	System.out.println("provider is " + provider.toString());
     	if (provider != null) {
     		location = locationManager.getLastKnownLocation(provider);
     		/* TODO - adjust update times to minimize battery use. Can be changed as needed.
     		*
     		*/
-    	} 
+    	}
+
+    	LocationListener locationListener = new LocationListener()
+    	{
+    		public void onLocationChanged(Location location)
+    		{
+    			System.out.println("Location has changed!!!");
+    			// Update application based on new location.
+    			updateWithNewLocation(location);
+    		}
+    		public void onProviderDisabled(String provider)
+    		{
+    			// Update application if provider is disabled.
+    			updateWithNewLocation(null);
+    		}
+    		public void onProviderEnabled(String provider)
+    		{
+    			// Update application if provider is enabled.
+    		}
+    		public void onStatusChanged(String provider, int status, Bundle extras)
+    		{
+    			// Update application if provider hardware status changed.
+    		}
+    	};
     	
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.map);
-        
-        mapView = (MapView) findViewById(R.id.mapView);
+    	//updateWithNewLocation(location);
+    	
+    	locationManager.requestLocationUpdates("gps", t, distance, locationListener);
+    	
+        mapView = (MapView)findViewById(R.id.mapView);
         mapView.setBuiltInZoomControls(true);
         
         mapController = mapView.getController();
         String coordinates[] = {"35.908138", "14.500975"};
         double lat = Double.parseDouble(coordinates[0]);
         double lng = Double.parseDouble(coordinates[1]);
-        System.out.println("GPS: " + location.getLatitude() + "," + location.getLongitude());
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
         
         gp = new GeoPoint((int)(lat * 1E6),(int)(lng * 1E6));
- 
+        
         mapController.animateTo(gp);
         mapController.setZoom(17); 
+
+        // Add overlay
+        moodOverlay = new MoodOverlay();
+        List<Overlay> overlays = mapView.getOverlays();
+        overlays.add(moodOverlay);
+        
         mapView.invalidate();
+    }
+    
+    private void updateWithNewLocation(Location location)
+    {
+    	Double lat,lng;
+    	if(location != null)
+    	{
+    		lat = location.getLatitude()*1E6;
+    		lng = location.getLongitude()*1E6;
+    		GeoPoint pt = new GeoPoint(lat.intValue(),lng.intValue());
+    		if(lat != 0.0 && lng != 0.0)
+    			mapController.animateTo(pt);
+    		System.out.println(lat + "x" + lng);
+    	}
+    	else
+    		System.out.println("No location.");
+    }
+    
+    private Criteria getCriteria()
+    {
+    	Criteria criteria = new Criteria();
+    	criteria.setAccuracy(Criteria.ACCURACY_FINE);
+    	criteria.setAltitudeRequired(false);
+    	criteria.setBearingRequired(false);
+    	criteria.setCostAllowed(true);
+    	criteria.setPowerRequirement(Criteria.POWER_LOW);
+    	
+    	return criteria;
     }
  
     @Override
--- drunkendroid/src/itu/malta/drunkendroid/ui/map/MoodOverlay.java
+++ drunkendroid/src/itu/malta/drunkendroid/ui/map/MoodOverlay.java
@@ -1,7 +1,55 @@
 package itu.malta.drunkendroid.ui.map;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.RectF;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
+import com.google.android.maps.Projection;
 
-public class MoodOverlay extends Overlay {
-
+public class MoodOverlay extends Overlay
+{
+	@Override
+	public void draw(Canvas canvas, MapView mapView, boolean shadow)
+	{
+		Projection projection = mapView.getProjection();
+		Double lat = 35.908138*1E6;
+		Double lng = 14.500975*1E6;
+		
+		GeoPoint gp = new GeoPoint(lat.intValue(), lng.intValue());
+		
+		if(shadow == false)
+		{
+			Point point = new Point();
+			projection.toPixels(gp, point);
+			
+			// Create and setup paint brush
+			Paint paint = new Paint();
+			paint.setARGB(250, 255, 0, 0);
+			paint.setAntiAlias(true);
+			paint.setFakeBoldText(true);
+			
+			// Create circle
+			int rad = 5;
+			RectF oval = new RectF(point.x-rad, point.y-rad,point.x+rad,point.y+rad);
+			
+			// Draw on the canvas
+			canvas.drawOval(oval, paint);
+			canvas.drawText("Red circle", point.x+rad, point.y+rad, paint);
+		}
+		else
+		{
+			
+		}
+	}
+	
+	@Override
+	public boolean onTap(GeoPoint geoPoint, MapView mapView)
+	{
+		// Return true if screen tap is handled by this overlay
+		return false;
+	}
 }
