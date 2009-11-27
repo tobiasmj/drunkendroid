bf0ecf073badc6d052164769290d9b1e211403d6
snielsen <snielsen@3995579f-fdfd-45ad-a057-9d0aee8b8447>
2009-11-27 10:41:21 +0000

    Implemented get and put methods.
    git-svn-id: http://subversion.assembla.com/svn/drunkendroid/src@151 3995579f-fdfd-45ad-a057-9d0aee8b8447

--- drunkendroid/src/itu/malta/drunkendroid/tech/IWebserviceConnection.java
+++ drunkendroid/src/itu/malta/drunkendroid/tech/IWebserviceConnection.java
@@ -3,6 +3,7 @@ package itu.malta.drunkendroid.tech;
 import org.apache.http.HttpResponse;
 
 public interface IWebserviceConnection {
-	public HttpResponse post(String URI, String xmlContent);
+	public HttpResponse post(String uri, String xmlContent);
 	public HttpResponse get(String string);
+	public HttpResponse put(String uri, String xmlContent);
 }
--- drunkendroid/src/itu/malta/drunkendroid/tech/WebserviceConnectionREST.java
+++ drunkendroid/src/itu/malta/drunkendroid/tech/WebserviceConnectionREST.java
@@ -2,22 +2,24 @@ package itu.malta.drunkendroid.tech;
 
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.util.AndroidRuntimeException;
 
 public class WebserviceConnectionREST implements IWebserviceConnection{
-	private static final String BASE_URI = "http://192.168.0.16:8182/drunkendroid/";
-	private static final String targetDomain = "192.168.0.16";
+	private static final String BASE_URI = "http://192.168.0.13:8182/drunkendroid/";
+	private static final String targetDomain = "192.168.0.13";
 	
-	public HttpResponse post(String URI, String xmlContent){
+	public HttpResponse post(String uri, String xmlContent){
 		DefaultHttpClient httpClient = new DefaultHttpClient();
         
         HttpHost targetHost = new HttpHost(targetDomain, 8182, "http");
         // Using POST here
-        HttpPost httpPost = new HttpPost(BASE_URI+URI);
+        HttpPost httpPost = new HttpPost(BASE_URI+uri);
         // Make sure the server knows what kind of a response we will accept
         httpPost.addHeader("Accept", "text/xml");
         // Also be sure to tell the server what kind of content we are sending
@@ -38,9 +40,52 @@ public class WebserviceConnectionREST implements IWebserviceConnection{
         }
 	}
 
-	public HttpResponse get(String string) {
-		// TODO Auto-generated method stub
-		throw new AndroidRuntimeException("Not yet implemented");
+	public HttpResponse get(String uri) {
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+        
+        HttpHost targetHost = new HttpHost(targetDomain, 8182, "http");
+        // Using POST here
+        HttpGet httpGet = new HttpGet(BASE_URI+uri);
+        // Make sure the server knows what kind of a response we will accept
+        httpGet.addHeader("Accept", "text/xml");
+        // Also be sure to tell the server what kind of content we are sending
+        httpGet.addHeader("Content-Type", "text/xml");
+                
+        try 
+        {
+            return httpClient.execute(targetHost, httpGet);
+       }
+        catch (Exception ex)
+        {
+                ex.printStackTrace();
+                return null;
+        }
+	}
+	
+	public HttpResponse put(String uri, String xmlContent){
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+        
+        HttpHost targetHost = new HttpHost(targetDomain, 8182, "http");
+        // Using POST here
+        HttpPut httpPut = new HttpPut(BASE_URI+uri);
+        // Make sure the server knows what kind of a response we will accept
+        httpPut.addHeader("Accept", "text/xml");
+        // Also be sure to tell the server what kind of content we are sending
+        httpPut.addHeader("Content-Type", "text/xml");
+                
+        try 
+        {
+            StringEntity entity = new StringEntity(xmlContent, "UTF-8");
+            entity.setContentType("text/xml");
+            httpPut.setEntity(entity);
+            
+            return httpClient.execute(targetHost, httpPut);
+         }
+        catch (Exception ex)
+        {
+                ex.printStackTrace();
+                return null;
+        }
 	}
 
 }
